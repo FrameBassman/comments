c8614388e7806b6cbda89f7215d60341e541646e
Дмитрий Ромашов <framebassman@gmail.com>
2018-09-07 11:53:25 +0300

    comments reducer doesnt handle unknown actions

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -1,16 +1,31 @@
 import { commentReducer } from "reducers/comments";
 import { SAVE_COMMENT } from 'actions/types';
 
+const initialComments = [];
+const newComment = 'Test';
+
 it('handle actions from SAVE_COMMENT', () => {
-  const initnalComments = [];
-  const newComment = 'Test';
   const action = {
     type: SAVE_COMMENT,
     payload: newComment,
   }
   
-  const updatedState = commentReducer(initnalComments, action);
+  const updatedState = commentReducer(initialComments, action);
 
   expect(updatedState).toContainEqual(newComment);
+});
+
+it('handle actions from unknown type', () => {
+  const UNKNOWN_TYPE = 'ASDAWDAFWA';
+  const action = {
+    type: UNKNOWN_TYPE,
+    payload: newComment,
+  }
+
+  const updatedState = commentReducer(initialComments, action);
+
+  expect(updatedState).toEqual(
+    expect.arrayContaining(initialComments)
+  );
 });
 
